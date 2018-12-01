a38e4520d0bf42458ae1c17c6f3bc19b2f0d5ff1
Amartya <amartyautkarsh@dhcp-rhodes-2692.eduroam.cornell.edu>
2018-12-01 15:39:45 -0500

    Updated functionality

--- src/routes.py
+++ src/routes.py
@@ -5,18 +5,17 @@ import requests
 from threading import Timer
 from constants import *
 from datetime import datetime as dt, date, timedelta
+import calendar
 
 db_filename = "todo.db"
 app = Flask(__name__)
 
-#times help format do
-
 # Run update and initial how?
 #imagelink check
-
-#cafes eateries use?
+#after deployment change?
 
 #hardcode all data
+##cafes eateries use one function to update all
 #deploy
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % db_filename
@@ -30,7 +29,7 @@ with app.app_context():
 
 def make_time_format(time):
   """
-  Takes in time string and convert it to the proper time format
+  Helper function that takes in time string and convert it to the proper time format
   """
   time = time.replace("pm", ":00 PM")
   time = time.replace("am", ":00 AM")
@@ -38,7 +37,7 @@ def make_time_format(time):
 
 def get_all_times(weeks):
   """
-  Gets array of times of 7 days from today
+  Helper function that gives array of times of 7 days from today
   """
   date_today = date.today()
   upper_date_bound = date_today + timedelta(days=7)
@@ -48,10 +47,25 @@ def get_all_times(weeks):
       times_date = dt.strptime(value[day]["date"], "%Y-%m-%d").date()
       if times_date >= date_today and times_date < upper_date_bound:
         result.append(make_time_format(value[day]["rendered"]))
-  print(result)
   return result
 
-
+def update_cafe():
+  """
+  Updates Cafe time
+  """
+  cafe_names = ["Amit Bhatia Libe Café"]
+  cafe_names_libraries = {"Amit Bhatia Libe Café": "Olin Library"}
+  jsons = requests.get("https://now.dining.cornell.edu/api/1.0/dining/eateries.json").json()
+  for value in jsons["data"]["eateries"]:
+    if value["name"] in cafe_names:
+      for day in value["operatingHours"]:
+        if day["date"] == str(date.today()):
+          time = day["events"][0]["start"]+" - "+ day["events"][0]["end"]
+          time = time.replace("am", " AM")
+          time = time.replace("pm", " PM")
+          record = Time.query.filter_by(name=cafe_names_libraries[value["name"]]).first()
+          record.information = [record.information[0], record.information[1], record.information[2], record.information[3], time, record.information[5]]
+          db.session.commit()
 
 
 @app.route('/api/initial/')
@@ -67,7 +81,6 @@ def initial():
     information = LIBRARY_INFORMATION[i],
     location = LIBRARY_LOCATION[i]
     )
-    print(i)
     db.session.add(record)
     db.session.commit()
   return json.dumps({'success': True, 'data': [post.serialize() for post in Time.query.all()]}), 200
@@ -79,16 +92,15 @@ def update():
   """
   times_json = requests.get(CORNELL_LIBRARY_TIMES_URL).json()
   try:
+
+    update_cafe()
     COUNTER = 0
     for value in times_json["locations"]:
-      # if value["name"] == "Manndible":
-      #     record = Time.query.filter_by(name="Mann Library").first()
-      #     record.information[4] = value["weeks"][0]["Sunday"]["rendered"]
-      #     db.session.commit()
-      # if value["name"] == "Amit Bhatia Libe Cafe":
-      #     record = Time.query.filter_by(name="Olin Library").first()
-      #     record.information[4] = value["weeks"][0]["Sunday"]["rendered"]
-      #     db.session.commit()
+      if value["name"] == "Manndible":
+          record = Time.query.filter_by(name="Mann Library").first()
+          cafe_time = value["weeks"][0][calendar.day_name[date.today().weekday()]]["rendered"]
+          record.information = [record.information[0], record.information[1], record.information[2], record.information[3], cafe_time, record.information[5]]
+          db.session.commit()
 
       record = Time.query.filter_by(json_name=value["name"]).first()
       if record is not None:
@@ -109,30 +121,6 @@ def get_times():
       'success': True,
       'data': { "libraries": [post.serialize() for post in Time.query.all()] }
     }), 200
-
-@app.route('/api/test/')
-def initirral():
-  """
-  Hardcoding fixed data
-  """
-  # times_json = requests.get(CORNELL_LIBRARY_TIMES_URL).json()
-  # for value in times_json["locations"]:
-  #   if value["name"] == "Manndible":
-  #       record = Time.query.filter_by(name="Mann Library").first()
-  #       info = record.information
-  #       info[4] = value["weeks"][0]["Sunday"]["rendered"]
-  #       record.information = info
-  #       print(record.information)
-  #       db.session.commit()
-  #       print(record.information)
-
-  times_date = dt.strptime('2018-12-02', "%Y-%m-%d").date()
-  date_today = date.today()
-  print("AFSFASFasjgfhzkbdwajfabkas")
-  # d.replace(days=d.da + 7)
-  print(make_time_format("2pm - 3pm"))
-  return json.dumps({'success': True, 'data': [Time.query.filter_by(name="Mann Library").first().information]}), 200
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
